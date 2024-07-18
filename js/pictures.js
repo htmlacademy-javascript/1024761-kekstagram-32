5ad252bd6e05618195a4d49dc8cb88c6d035e874
Irina Makarova <mario-2812@yandex.ru>
2024-07-18 17:15:18 +0300

    Добавляет склонированный шаблон последним элементом в блок

--- js/pictures.js
+++ js/pictures.js
@@ -3,9 +3,12 @@ const PICTURE_TEMPLATE = document.querySelector('#picture').content.querySelecto
 const PICTURE = CLONED_PICTURE;
 
 const CLONED_PICTURE = PICTURE_TEMPLATE.cloneNode(true);
-CLONED_PICTURE.querySelector('.picture_img').src = PICTURE.url;
-CLONED_PICTURE.querySelector('.picture_img').alt = PICTURE.description;
-CLONED_PICTURE.querySelector('.picture_likes') = PICTURE.likes;
+CLONED_PICTURE.querySelector('.picture__img').src = PICTURE.url;
+CLONED_PICTURE.querySelector('.picture__img').alt = PICTURE.description;
+CLONED_PICTURE.querySelector('.picture__likes') = PICTURE.likes;
 CLONED_PICTURE.querySelector('.picture__comments') = PICTURE.comments.length;
 
+
+
+PICTURES.appendChild(CLONED_PICTURE);
 
