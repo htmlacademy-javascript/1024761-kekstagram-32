1cd37f9d32172245db9978920769bd66d93f3e41
Irina Makarova <mario-2812@yandex.ru>
2024-07-18 17:41:08 +0300

    Сщздает цикл для массива данных

--- js/pictures.js
+++ js/pictures.js
@@ -1,14 +1,15 @@
+import {createArrayPhoto} from './data.js';
+
 const PICTURES = document.querySelector('.pictures');
 const PICTURE_TEMPLATE = document.querySelector('#picture').content.querySelector('picture');
-const PICTURE = CLONED_PICTURE;
+const arrayPicture = createArrayPhoto();
 
-const CLONED_PICTURE = PICTURE_TEMPLATE.cloneNode(true);
-CLONED_PICTURE.querySelector('.picture__img').src = PICTURE.url;
-CLONED_PICTURE.querySelector('.picture__img').alt = PICTURE.description;
-CLONED_PICTURE.querySelector('.picture__likes') = PICTURE.likes;
-CLONED_PICTURE.querySelector('.picture__comments') = PICTURE.comments.length;
-
-
-
-PICTURES.appendChild(CLONED_PICTURE);
+arrayPicture.forEach((PICTURE) => {
+  const CLONED_PICTURE = PICTURE_TEMPLATE.cloneNode(true);
+  CLONED_PICTURE.querySelector('.picture__img').src = PICTURE.url;
+  CLONED_PICTURE.querySelector('.picture__img').alt = PICTURE.description;
+  CLONED_PICTURE.querySelector('.picture__likes').textContent = PICTURE.likes;
+  CLONED_PICTURE.querySelector('.picture__comments').textContent = PICTURE.comments.length;
+  PICTURES.appendChild(CLONED_PICTURE);
+});
 
