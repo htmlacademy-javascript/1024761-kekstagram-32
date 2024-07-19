abfbb3d412c732feecfd6146091bab2b7e60fa84
Irina Makarova <mario-2812@yandex.ru>
2024-07-19 21:41:17 +0300

    Исправляет ошибки после созвона

--- js/pictures.js
+++ js/pictures.js
@@ -1,17 +1,17 @@
 import {createArrayPhoto} from './data.js';
 
-const PICTURES = document.querySelector('.pictures');
-const PICTURE_TEMPLATE = document.querySelector('#picture').content.querySelector('picture');
+const pictures = document.querySelector('.pictures');
+const picturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const arrayPicture = createArrayPhoto();
 const fragment = document.createDocumentFragment();
 
-arrayPicture.forEach((PICTURE) => {
-  const CLONED_PICTURE = PICTURE_TEMPLATE.cloneNode(true);
-  CLONED_PICTURE.querySelector('.picture__img').src = PICTURE.url;
-  CLONED_PICTURE.querySelector('.picture__img').alt = PICTURE.description;
-  CLONED_PICTURE.querySelector('.picture__likes').textContent = PICTURE.likes;
-  CLONED_PICTURE.querySelector('.picture__comments').textContent = PICTURE.comments.length;
-  fragment.appendChild(CLONED_PICTURE);
+arrayPicture.forEach((picture) => {
+  const clonedPicture = picturesTemplate.cloneNode(true);
+  clonedPicture.querySelector('.picture__img').src = picture.url;
+  clonedPicture.querySelector('.picture__img').alt = picture.description;
+  clonedPicture.querySelector('.picture__likes').textContent = picture.likes;
+  clonedPicture.querySelector('.picture__comments').textContent = picture.comments.length;
+  fragment.appendChild(clonedPicture);
 });
 
-PICTURES.appendChild(fragment);
+pictures.appendChild(fragment);
