1c56d40dd3b0769bdd62122483b72e5e02eef818
Irina Makarova <mario-2812@yandex.ru>
2024-07-24 17:54:51 +0300

    Создает атрибуты комментария по шаблону

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import './utils.js';
 import './pictures.js';
-import {createArrayPhoto} from './data.js';
-import { renderPosts } from './pictures.js';
+import './modal-picture.js';
 
+import {createArrayPhoto} from './data.js';
+import { randerGalery } from './modal-picture.js';
+randerGalery();
+import { renderPosts } from './pictures.js';
 renderPosts (createArrayPhoto());
