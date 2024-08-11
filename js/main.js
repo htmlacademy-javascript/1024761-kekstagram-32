541a24a19abcceadaab466edaf79e1f68be35687
Irina Makarova <mario-2812@yandex.ru>
2024-08-11 15:49:57 +0300

    Выводит по 5 комментариев под картинкой

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 import {dataPhoto} from './data.js';
-import { randerGallery } from './modal-picture.js';
-import { showBigPicture } from './modal-picture.js';
+import { renderGallery } from './modal-picture.js';
 import { renderPosts } from './pictures.js';
-//renderPosts(dataPhoto);
-randerGallery(dataPhoto);
-//showBigPicture();
-console.log(dataPhoto)
+renderPosts(dataPhoto);
+renderGallery(dataPhoto);
+
--- js/modal-picture.js
+++ js/modal-picture.js
@@ -1,14 +1,20 @@
 import { renderPosts } from './pictures.js';
 
+const SHOW_COUNT_COMMENTS = 5;
+
 const picturesContainer = document.querySelector('.pictures');
 const bigPicture = document.querySelector('.big-picture');
-const commentCount = bigPicture.querySelector('.social__comment-count');
+//const commentCount = bigPicture.querySelector('.social__comment-count');
+const commentShowCount = bigPicture.querySelector('.social__comment-shown-count');
+const commentTotalCount = bigPicture.querySelector('.social__comment-total-count');
 const commentsList = bigPicture.querySelector('.social__comments');
 const commentElement = document.querySelector('#comment').content.querySelector('.social__comment');
 const commentsLoader = bigPicture.querySelector('.social__comments-loader');
 const body = document.querySelector('body');
 const buttonClose = bigPicture.querySelector('.big-picture__cancel');
 
+let commentsShown = 0;
+let comments = [];
 
 function createComment ({avatar, name, message}) {
   const clonedComment = commentElement.cloneNode(true);
@@ -20,32 +26,53 @@ function createComment ({avatar, name, message}) {
   return clonedComment;
 }
 
-const renderComments = (comments) => {
-  commentsList.innerHTML = '';
+//const renderComments = (comments) => {
+//  commentsList.innerHTML = '';
+
+const renderComments = () => {
+  commentsShown += SHOW_COUNT_COMMENTS;
+
+  if (commentsShown >= comments.length) {
+    commentsLoader.classList.add('hidden');
+    commentsShown = comments.length;
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
 
   const fragment = document.createDocumentFragment();
-  comments.forEach((item) => {
-    const clonedComment = createComment(item);
+  //comments.forEach((item) => {
+  //  const clonedComment = createComment(item);
+  for (let i = 0; i < commentsShown; i++) {
+    const clonedComment = createComment(comments[i]);
     fragment.append(clonedComment);
-  });
+  };
+  
+  commentsList.innerHTML = '';
   commentsList.append(fragment);
+  commentShowCount.textContent = commentsShown;
+  commentTotalCount.textContent = comments.length;
 };
 
-function removeBigPicture () {
+function closeBigPicture () {
   bigPicture.classList.add('hidden');
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
-}
+  commentsShown = 0;
+};
 
 function onDocumentKeydown(evt) {
   if (evt.key === 'Escape') {
     evt.preventDefault();
-    removeBigPicture ();
+    closeBigPicture ();
   }
-}
+};
 
 function onButtonCloseClick () {
-  removeBigPicture ();
+  closeBigPicture ();
+};
+
+function onCommentsLoaderClick() {
+  renderComments()
 }
 
 const renderPictureDetails = ({url, likes, description}) => {
@@ -59,36 +86,41 @@ const showBigPicture = (data) => {
   bigPicture.classList.remove('hidden');
   body.classList.add('modal-open');
   commentsLoader.classList.add('hidden');
-  commentCount.classList.add('hidden');
+  //commentCount.classList.add('hidden');
   document.addEventListener('keydown', onDocumentKeydown);
-
+  
   renderPictureDetails(data);
-  renderComments(data.comments);
+  comments = data.comments;
+  //renderComments(data.comments);
+  if (comments.length > 0) {
+    renderComments();
+  }
 };
 
 buttonClose.addEventListener('click', onButtonCloseClick);
+commentsLoader.addEventListener('click', onCommentsLoaderClick);
+
 export {showBigPicture};
 
 //Перенести функцию в другой модуль
-function randerGallery(pictures) {
+function renderGallery(pictures) {
   picturesContainer.addEventListener('click', (evt) => {
     const clonedPicture = evt.target.closest('.picture');
-    console.log(clonedPicture);
     if (!clonedPicture) {
       return;
     }
 
-   // evt.preventDefault();
+    // evt.preventDefault();
     const picture = pictures.find(
       (item) => item.id === +clonedPicture.dataset.pictureId
     );
     if (picture) {
-    showBigPicture(picture);
+      showBigPicture(picture);
     }
   });
 
   renderPosts(pictures, picturesContainer);
 }
 
-export {randerGallery};
+export {renderGallery};
 
