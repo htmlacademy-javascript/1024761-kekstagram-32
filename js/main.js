b0474da541d06d1771e21060ecd31350f556f963
Irina Makarova <mario-2812@yandex.ru>
2024-08-11 20:09:27 +0300

    Исправляет ошибку генерации картинок

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 import {dataPhoto} from './data.js';
 import { renderGallery } from './modal-picture.js';
-import { renderPosts } from './pictures.js';
-renderPosts(dataPhoto);
 renderGallery(dataPhoto);
 
