9cb724d446b556165f5bd52f51d5cbec6a71cd7c
Irina Makarova <mario-2812@yandex.ru>
2024-08-15 14:12:29 +0300

    Находит все задействаванные элементы

--- js/scale.js
+++ js/scale.js
@@ -2,3 +2,10 @@ const SCALE_STEP = 25;
 const MIN_SCALE = 25;
 const MAX_SCALE = 100;
 const DEFAULT_SCALE = 100;
+
+const modalElement = document.querySelector('.img-upload');
+const smallerButtonElement = document.querySelector('.scale__control--smaller');
+const biggerButtonElement = document.querySelector('.scale__control--bigger');
+const scaleInputElement = document.querySelector('.scale__control--value');
+const imageElement = document.querySelector('.img-uploadimg-upload__preview img');
+
